4a9252507f0a23238af1cc5610af07d743894cb8
Speed C <speedchanjun@gmail.com>
2020-03-11 14:30:00 +1000

    Merged nav drawer & mobile nav, fixed width issue on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <v-app>
     <Navigation v-if="current_user"></Navigation>
-    <NavigationMobile v-if="current_user" @is_display_mobile_nav="toggle_drawer_visibility"></NavigationMobile>
-    <NavigationDrawer v-if="current_user" :is_drawer_visible="is_drawer_visible"></NavigationDrawer>
+    <NavigationMobile v-if="current_user"></NavigationMobile>
     <v-content class="bg_coral">
       <router-view/>
     </v-content>
@@ -12,12 +11,11 @@
 <script>
   import Navigation from "./views/Navigation";
   import NavigationMobile from "./views/NavigationMobile";
-  import NavigationDrawer from "./views/NavigationDrawer";
   import { mapState } from 'vuex';
 
   export default {
     name: 'App',
-    components: { Navigation, NavigationDrawer, NavigationMobile },
+    components: { Navigation, NavigationMobile },
     computed: {
       ...mapState(['current_user'])
     },
--- src/views/NavigationMobile.vue
+++ src/views/NavigationMobile.vue
@@ -1,40 +1,93 @@
 <template>
-  <v-app-bar app dark class="hidden-md-and-up">
-    <v-toolbar-title>Comparater Mobile</v-toolbar-title>
-    <v-spacer></v-spacer>
+  <v-container app>
+    <v-app-bar app dark class="hidden-md-and-up">
+      <v-toolbar-title>Comparater Mobile</v-toolbar-title>
+      <v-spacer></v-spacer>
 
-    <v-app-bar-nav-icon @click="toggle_mobile_nav_display"></v-app-bar-nav-icon>
-  </v-app-bar>
+      <v-app-bar-nav-icon @click="toggle_mobile_nav_display"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer absolute temporary right v-model="is_display_mobile_nav">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Comparater</v-list-item-title>
+          <v-list-item-subtitle>Compare, rate, vote!</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list nav>
+        <v-list-item to="/dashboard">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item :to="{ name: 'UserDetail', params: { user_uid: current_user.uid }}">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item to="/settings">
+          <v-list-item-icon>
+            <v-icon>mdi-account-details</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item @click="logout">
+          <v-list-item-icon>
+            <v-icon>mdi-logout-variant</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
 </template>
 
 <script>
-  const fb = require('../firebaseConfig.js');
-  import { mapState } from 'vuex';
+const fb = require("../firebaseConfig.js");
+import { mapState } from "vuex";
 
-  export default {
-    data() {
-      return {
-        is_display_mobile_nav: false
-      }
-    },
-    methods: {
-      logout() {
-        fb.auth.signOut().then(() => {
-          this.$store.dispatch('clear_current_user_data');
-          this.$router.push('/login');
-        }).catch(err => {
-          console.log(err)
+export default {
+  data() {
+    return {
+      is_display_mobile_nav: false
+    };
+  },
+  methods: {
+    logout() {
+      fb.auth
+        .signOut()
+        .then(() => {
+          this.$store.dispatch("clear_current_user_data");
+          this.$router.push("/login");
         })
-      },
-      toggle_mobile_nav_display() {
-        this.is_display_mobile_nav = !this.is_display_mobile_nav;
-        this.$emit('is_display_mobile_nav', this.is_display_mobile_nav);
-      }
+        .catch(err => {
+          console.log(err);
+        });
     },
-    computed: {
-      ...mapState({
-        current_user: 'current_user'
-      })
+    toggle_mobile_nav_display() {
+      this.is_display_mobile_nav = !this.is_display_mobile_nav;
     }
+  },
+  computed: {
+    ...mapState({
+      current_user: "current_user"
+    })
   }
+};
 </script>
